52b188a85eedf0e3106a6f6922d88667fd5edab3
Dolan Miu <dolan_miu@hotmail.com>
2023-07-22 20:17:57 +0100

    Update with more cases

--- process/src/spam-checker/imitated-english-check.spec.ts
+++ process/src/spam-checker/imitated-english-check.spec.ts
@@ -9,6 +9,11 @@ describe("hasImitatedEnglishCharacters", () => {
 
   it("should work", () => {
     expect(hasImitatedEnglishCharacters("--ᗰᑕᗩᖴee®")).toBe(true);
+    expect(hasImitatedEnglishCharacters("ᗰ")).toBe(true);
+    expect(hasImitatedEnglishCharacters("ᑕ")).toBe(true);
+    expect(hasImitatedEnglishCharacters("ᗩ")).toBe(true);
+    expect(hasImitatedEnglishCharacters("ᖴ")).toBe(true);
+
   });
 
   it("should work", () => {
@@ -43,5 +48,53 @@ describe("hasImitatedEnglishCharacters", () => {
     expect(hasImitatedEnglishCharacters("Tʜɪꜱ ɪꜱ ᴀ ᴍɪꜱᴄᴇʟʟᴀɴᴇᴏᴜꜱ ᴛᴇꜱᴛ")).toBe(
       true,
     );
+  });
+
+  it("should work", () => {
+    expect(hasImitatedEnglishCharacters("𝒜𝓑𝓒𝒟")).toBe(true);
+  });
+
+  it("should work", () => {
+    expect(hasImitatedEnglishCharacters("𝒜𝓑𝓒𝒟")).toBe(true);
+  });
+
+  it("should work", () => {
+    expect(hasImitatedEnglishCharacters("ǎ")).toBe(true);
+  });
+
+//   it("should work", () => {
+//     expect(hasImitatedEnglishCharacters("ϭ")).toBe(true);
+//   });
+
+//   it("should work", () => {
+//     expect(hasImitatedEnglishCharacters("д")).toBe(true);
+//   });
+
+  it("should work", () => {
+    expect(hasImitatedEnglishCharacters("ℓΩ℧ℵℶℷℸℼℽℾ")).toBe(true);
+  });
+
+  it("should work", () => {
+    expect(hasImitatedEnglishCharacters("⁰")).toBe(true);
+  });
+
+  it("should work", () => {
+    expect(hasImitatedEnglishCharacters("Ⓐⓐⓐⓑⓒⓓⓔⓕⓖⓗⓘⓙⓚⓛⓜⓝⓞ")).toBe(true);
+  });
+
+  it("should work", () => {
+    expect(hasImitatedEnglishCharacters("╚")).toBe(true);
+  });
+
+  it("should work", () => {
+    expect(hasImitatedEnglishCharacters("ⁱ⁰ⁿ⁺⁻⁼⁽⁾ⁿᵖ")).toBe(true);
+  });
+
+//   it("should work", () => {
+//     expect(hasImitatedEnglishCharacters("⒜⒝⒞⒟⒠⒡⒢⒣⒤⒥⒦⒧⒨⒩⒪⒫⒬⒭⒮⒯")).toBe(true);
+//   });
+
+  it("should work", () => {
+    expect(hasImitatedEnglishCharacters("Regular normal text: Hello World")).toBe(false);
   });
 });
--- process/src/spam-checker/imitated-english-check.ts
+++ process/src/spam-checker/imitated-english-check.ts
@@ -29,7 +29,7 @@ export const hasImitatedEnglishCharacters = (input: string): boolean => {
   // Define a regular expression pattern to match Greek letters that resemble English letters.
   const greekLettersPattern = /[\u0391-\u03A2]/;
 
-  // Define a regular expression pattern to match Cyrillic characters that resemble English letters.
+  // Define a regular expression pattern to match Cyrillic letters that resemble English letters.
   const cyrillicLettersPattern = /[\u0421-\u0425]/;
 
   // Define a regular expression pattern to match common ligatures like "æ" or "œ".
@@ -40,6 +40,31 @@ export const hasImitatedEnglishCharacters = (input: string): boolean => {
 
   // Define a regular expression pattern to match Latin characters with modifiers (superscripts and subscripts).
   const latinCharactersWithModifiersPattern = /[\u207F\u1D2C-\u1D61]/;
+
+  // Define a regular expression pattern to match mathematical alphanumeric symbols.
+  const mathematicalAlphanumericSymbolsPattern =
+    /[\uD835\uDC00\uD835\uDC01\uD835\uDC02\uD835\uDC03\uD835\uDC04\uD835\uDC05\uD835\uDC06\uD835\uDC07\uD835\uDC08\uD835\uDC09\uD835\uDC0A\uD835\uDC0B\uD835\uDC0C\uD835\uDC0D\uD835\uDC0E\uD835\uDC0F\uD835\uDC10\uD835\uDC11\uD835\uDC12\uD835\uDC13\uD835\uDC14\uD835\uDC15\uD835\uDC16\uD835\uDC17\uD835\uDC18\uD835\uDC19\uD835\uDC1A\uD835\uDC1B\uD835\uDC1C\uD835\uDC1D\uD835\uDC1E\uD835\uDC1F\uD835\uDC20\uD835\uDC21\uD835\uDC22\uD835\uDC23\uD835\uDC24\uD835\uDC25\uD835\uDC26\uD835\uDC27\uD835\uDC28\uD835\uDC29\uD835\uDC2A\uD835\uDC2B\uD835\uDC2C\uD835\uDC2D\uD835\uDC2E\uD835\uDC2F\uD835\uDC30\uD835\uDC31\uD835\uDC32\uD835\uDC33\uD835\uDC9C\uD835\uDC9D\uD835\uDC9E\uD835\uDC9F]/;
+
+  // Define a regular expression pattern to match Latin-1 Supplement characters.
+  const latin1SupplementPattern =
+    /[\u01CE\u00E5\u00DF\u00F0\u014B\u0153\u017F\u0192\u0254\u0283\u0292]/;
+
+  // Define a regular expression pattern to match letterlike symbols.
+  const letterlikeSymbolsPattern =
+    /[\u2113\u210F\u2126\u2127\u2135-\u2138\u213C-\u213E]/;
+
+  // Define a regular expression pattern to match mathematical symbols (superscripts).
+  const mathematicalSymbolsPattern =
+    /[\u2070\u2071\u207F\u207A-\u207E\u207F\u1D59]/;
+
+  // Define a regular expression pattern to match enclosed alphanumerics.
+  const enclosedAlphanumericsPattern = /[\u24B6-\u24CF\u24D0-\u24E9]/;
+
+  // Define a regular expression pattern to match box drawing characters.
+  const boxDrawingCharactersPattern = /[\u255A]/;
+
+  // Define a regular expression pattern to match the Canadian Syllabics range.
+  const canadianSyllabicsPattern = /[\u1400-\u167F]/;
 
   // Test if the input string contains any character that matches any of the individual patterns.
   // If any character from these scripts is found in the input, the test will return true, indicating the presence of imitated English characters.
@@ -57,6 +82,13 @@ export const hasImitatedEnglishCharacters = (input: string): boolean => {
     cyrillicLettersPattern.test(input) ||
     ligaturesPattern.test(input) ||
     phoneticExtensionsPattern.test(input) ||
-    latinCharactersWithModifiersPattern.test(input)
+    latinCharactersWithModifiersPattern.test(input) ||
+    mathematicalAlphanumericSymbolsPattern.test(input) ||
+    latin1SupplementPattern.test(input) ||
+    letterlikeSymbolsPattern.test(input) ||
+    mathematicalSymbolsPattern.test(input) ||
+    enclosedAlphanumericsPattern.test(input) ||
+    canadianSyllabicsPattern.test(input) ||
+    boxDrawingCharactersPattern.test(input)
   );
 };
